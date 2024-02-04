528c13d5e65b1634bb06d6dffca41f88b03bfe72
Yzagnoev123 <yzagnoev@digital-mr.com>
2024-02-04 17:11:16 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    int b = 1;
+    int c = 2;
+    int * a = &b;
+    int *d = &c;
+    vector<int*>vec = {a,d};
+    cout << vec.at(0);
+}
